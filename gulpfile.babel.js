fcd07f2674bcd3402bb4a22f2281d4139a635025
Bliss Man <blissman@gmail.com>
2018-12-09 10:33:35 -0500

    add eslint

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const Server = require('karma').Server;
+const eslint = require('gulp-eslint');
 
 gulp.task('test', function(done) {
     new Server({
@@ -12,6 +13,18 @@ gulp.task('tdd', function(done) {
     new Server({
         configFile: __dirname + '/karma.conf.js'
     }, done).start();
+});
+
+gulp.task('lint', function() {
+    return gulp.src('src/**').pipe(eslint({
+            'rules': {
+                'quotes': [1, 'single'],
+                'semi': [1, 'always']
+            }
+        }))
+        .pipe(eslint.format())
+        // Brick on failure to be super strict
+        .pipe(eslint.failOnError());
 });
 
 function defaultTask(cb) {
