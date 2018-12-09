2f6224ea5c06d64d7ab00bde77301a0ed14b680a
Bliss Man <blissman@gmail.com>
2018-12-09 10:14:43 -0500

    add karma, jasmine, tests

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,22 @@
+const gulp = require('gulp');
+const Server = require('karma').Server;
+
+gulp.task('test', function(done) {
+    new Server({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true
+    }, done).start();
+});
+
+gulp.task('tdd', function(done) {
+    new Server({
+        configFile: __dirname + '/karma.conf.js'
+    }, done).start();
+});
+
 function defaultTask(cb) {
-  // place code for your default task here
-  cb();
+    // place code for your default task here
+    cb();
 }
 
 exports.default = defaultTask
