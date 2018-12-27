b0785a9dab842d43c1030748830e03725478e80d
Bliss Man <blissman@gmail.com>
2018-12-27 10:44:57 -0500

    #10 add image minify code

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,6 +9,8 @@ const pump = require('pump');
 const cleanCSS = require('gulp-clean-css');
 // minify (html)
 const htmlmin = require('gulp-htmlmin');
+// minify (images)
+const imagemin = require('gulp-imagemin');
 // babel
 const babel = require('gulp-babel');
 // eslint
@@ -99,7 +101,7 @@ gulp.task('uglify-js', function(callback) {
     gulp minify (css) task
 */
 gulp.task('minify-css', () => {
-    return gulp.src('styles/*.css')
+    return gulp.src('src/styles/*.css')
         .pipe(cleanCSS({
             compatibility: '*',
             level: 2
@@ -119,6 +121,34 @@ gulp.task('minify-html', () => {
 });
 
 /*
+    gulp minify (images) task
+*/
+gulp.task('minify-images', () =>
+    gulp.src('src/images/*')
+    .pipe(imagemin([
+        imagemin.gifsicle({
+            interlaced: true
+        }),
+        imagemin.jpegtran({
+            progressive: true
+        }),
+        imagemin.optipng({
+            optimizationLevel: 5
+        }),
+        imagemin.svgo({
+            plugins: [{
+                    removeViewBox: true
+                },
+                {
+                    cleanupIDs: false
+                }
+            ]
+        })
+    ]))
+    .pipe(gulp.dest('dist/images'))
+);
+
+/*
     gulp-babel task
 */
 gulp.task('babel', () =>
@@ -130,4 +160,4 @@ gulp.task('babel', () =>
 );
 
 
-gulp.task('default', gulp.series('lint', 'babel', gulp.parallel('uglify-js', 'minify-css', 'minify-html'), 'test'));
+gulp.task('default', gulp.series('lint', 'babel', gulp.parallel('uglify-js', 'minify-css', 'minify-html', 'minify-images'), 'test'));
