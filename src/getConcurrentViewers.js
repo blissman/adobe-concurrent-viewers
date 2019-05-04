4740f704b6dad686768204788fdf379e69659c36
Bliss Man <blissman@gmail.com>
2019-05-04 12:19:21 -0400

    get queue working

--- src/getConcurrentViewers.js
+++ src/getConcurrentViewers.js
@@ -8,32 +8,58 @@ const testHTML = `
 </body>
 </html>
 `;
-const { JSDOM } = require( 'jsdom' );
-const jsdom = new JSDOM( testHTML );
-const { window } = jsdom;
-const { document } = window;
+const {
+    JSDOM
+} = require('jsdom');
+const jsdom = new JSDOM(testHTML);
+const {
+    window
+} = jsdom;
+const {
+    document
+} = window;
 global.window = window;
 global.document = document;
-const $ = global.jQuery = require( 'jquery' );
+const $ = global.jQuery = require('jquery');
 require("./wsse.js");
 require("./marketing_cloud.js");
 require("./user.js");
+require("./report.js");
 
-/*
-    These are the mandatory inputs
-*/
-/*********************************************************************/
-const segmentId = "someSegmentId"; // insert your segment id here
-/*********************************************************************/
-/*
-    Optional configuration constants (you shouldn't need to worry about these unless something's wrong with your query)
-*/
+const rsid = window.report.rsid; // your RSID name
+const segmentId = window.report.segmentId; // insert your segment id here
+const startDate = window.report.startDate; // your start date in YYYY-MM-DD format
+const endDate = window.report.endDate; // your end date in YYYY-MM-DD format
 const userName = window.user.name;
 const sharedSecret = window.user.sharedSecret;
-const method = "Company.GetReportSuites"; // determines the type of API request (you can leave this alone)
+const method = "Report.Get"; // determines the type of API request (you can leave this alone)
 const endpoint = "api.omniture.com"; // Adobe's San Jose datacentre (api2.omniture.com = Dallas, api3.omniture.com = London, api4.omniture.com = Singapore)
-const body = {}; // the request body (input your report parameters here)
+let reportID = "";
+const body = {
+    "reportID": reportID.reportID,
+    "reportDescription": {
+        "reportSuiteID": rsid,
+        "dateFrom": startDate,
+        "dateTo": endDate,
+        "metrics": [{
+            "id": "instances"
+        }],
+        "elements": [{
+            "id": "videoconcurrentviewers",
+            "top": "2880"
+        }],
+        // "segments": [{
+        //     "id": segmentId
+        // }],
+        "sortBy": "instances",
+        "locale": "en_US"
+    }
+};
 
-window.MarketingCloud.makeRequest(userName, sharedSecret, method, body, endpoint, function(e) {
-    console.log(e.responseText)
+window.MarketingCloud.makeRequest(userName, sharedSecret, "Report.Queue", body, endpoint, function(e) {
+    reportID = JSON.parse(e.responseText).reportID;
 });
+
+// window.MarketingCloud.makeRequest(userName, sharedSecret, method, body, endpoint, function(e) {
+//     console.log(e.responseText)
+// });
---
+++ src/reportTemplate.js
@@ -0,0 +1,6 @@
+window.report = {
+    rsid: "exampleRSID", // your RSID name
+    segmentId: "someSegmentId", // insert your segment id here
+    startDate: "2018-07-09", // your start date in YYYY-MM-DD format
+    endDate: "2018-07-10" // your end date in YYYY-MM-DD format
+};
