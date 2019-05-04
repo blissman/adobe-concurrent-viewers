31d9249f357df79bc6354cc78536d103a1772d92
Bliss Man <blissman@gmail.com>
2019-05-04 11:03:51 -0400

    add comments to make more user-friendly

--- src/getConcurrentViewers.js
+++ src/getConcurrentViewers.js
@@ -11,22 +11,24 @@ const testHTML = `
 `;
 const { JSDOM } = require( 'jsdom' );
 const jsdom = new JSDOM( testHTML );
-
-// Set window and document from jsdom
 const { window } = jsdom;
 const { document } = window;
-// Also set global window and document before requiring jQuery
 global.window = window;
 global.document = document;
-
 const $ = global.jQuery = require( 'jquery' );
-
-// require("./wsse.js");
 require("./marketing_cloud.js");
 
+/*
+    These are the mandatory inputs
+*/
+/*********************************************************************/
 const userName = "userName"; // insert your user name in here
 const sharedSecret = "sharedSecret"; // insert your shared secret here
 const segmentId = "someSegmentId"; // insert your segment id here
+/*********************************************************************/
+/*
+    Optional configuration constants (you shouldn't need to worry about these unless something's wrong with your query)
+*/
 const method = "Company.GetReportSuites"; // determines the type of API request (you can leave this alone)
 const endpoint = "api.omniture.com"; // Adobe's San Jose datacentre (api2.omniture.com = Dallas, api3.omniture.com = London, api4.omniture.com = Singapore)
 const body = {}; // the request body (input your report parameters here)
