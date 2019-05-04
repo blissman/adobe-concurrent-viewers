a2f4b8fe97a9d77eeb2acb5bbea6bef6d88c3c66
Bliss Man <blissman@gmail.com>
2019-05-04 11:10:13 -0400

    break out user

--- src/getConcurrentViewers.js
+++ src/getConcurrentViewers.js
@@ -17,18 +17,19 @@ global.window = window;
 global.document = document;
 const $ = global.jQuery = require( 'jquery' );
 require("./marketing_cloud.js");
+require("./user.js");
 
 /*
     These are the mandatory inputs
 */
 /*********************************************************************/
-const userName = "userName"; // insert your user name in here
-const sharedSecret = "sharedSecret"; // insert your shared secret here
 const segmentId = "someSegmentId"; // insert your segment id here
 /*********************************************************************/
 /*
     Optional configuration constants (you shouldn't need to worry about these unless something's wrong with your query)
 */
+const userName = window.user.name;
+const sharedSecret = window.user.sharedSecret;
 const method = "Company.GetReportSuites"; // determines the type of API request (you can leave this alone)
 const endpoint = "api.omniture.com"; // Adobe's San Jose datacentre (api2.omniture.com = Dallas, api3.omniture.com = London, api4.omniture.com = Singapore)
 const body = {}; // the request body (input your report parameters here)
---
+++ src/userTemplate.js
@@ -0,0 +1,4 @@
+window.user = {
+    name: "userName", // insert your user name in here
+    sharedSecret: "sharedSecret" // insert your shared secret here
+}
