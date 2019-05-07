b67cf3ba2c2c72aa246019714960d039b8e73c89
Man, Bliss <bliss.man@bellmedia.ca>
2019-05-07 12:16:35 -0400

    add example values to reportTemplate

--- src/config/reportTemplate.js
+++ src/config/reportTemplate.js
@@ -1,6 +1,8 @@
 window.report = {
     rsid: "exampleRSID", // your RSID name
     segmentId: "someSegmentId", // insert your segment id here
+    type: "daily", // can be daily or monthly
+    month: "05", // 01-12 representing the month for monthly reports; not used for daily reporting
     startDate: "2018-07-09", // your start date in YYYY-MM-DD format
     endDate: "2018-07-10", // your end date in YYYY-MM-DD format
     endpoint: "api.omniture.com" // Adobe's San Jose datacentre (api2.omniture.com = Dallas, api3.omniture.com = London, api4.omniture.com = Singapore)
