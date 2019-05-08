3a4a9351f426fddf1c27a4f2bccfa38ab5f9d2af
Bliss Man <blissman@gmail.com>
2019-05-08 17:02:41 -0400

    get monthly reports working

--- src/businessLogic.js
+++ src/businessLogic.js
@@ -30,39 +30,91 @@ window.parseData = {
 };
 
 window.getReport = {
+    reportValue: "",
     requestBody: (reportConfig) => {
         if (!reportConfig || typeof(reportConfig) !== "object") {
             console.log("Error: report config object is not ready!");
             return false;
         }
+        const type = reportConfig.type;
         let startDate;
         let endDate;
+        const bodyArray = [];
         const rsid = reportConfig.rsid;
         const segmentId = reportConfig.segmentId;
-        if (reportConfig.type === "daily") {
+        if (type === "daily") {
             startDate = reportConfig.startDate;
             endDate = reportConfig.endDate;
-        }
-        const body = {
-            "reportDescription": {
-                "reportSuiteID": rsid,
-                "dateFrom": startDate,
-                "dateTo": endDate,
-                "metrics": [{
-                    "id": "instances"
-                }],
-                "elements": [{
-                    "id": "videoconcurrentviewers",
-                    "top": "2880"
-                }],
-                "segments": [{
-                    "id": segmentId
-                }],
-                "sortBy": "instances",
-                "locale": "en_US"
+            const body = {
+                "reportDescription": {
+                    "reportSuiteID": rsid,
+                    "dateFrom": startDate,
+                    "dateTo": endDate,
+                    "metrics": [{
+                        "id": "instances"
+                    }],
+                    "elements": [{
+                        "id": "videoconcurrentviewers",
+                        "top": "2880"
+                    }],
+                    "segments": [{
+                        "id": segmentId
+                    }],
+                    "sortBy": "instances",
+                    "locale": "en_US"
+                }
+            };
+            bodyArray.push(body);
+        } else if (type === "monthly") {
+            const reportYear = reportConfig.year;
+            const reportMonth = reportConfig.month;
+            const totalDays = window.utils.getDays(reportMonth, reportYear);
+            const yearValue = reportYear.toString();
+            let monthValue;
+            if (reportMonth.toString().length === 1) {
+                monthValue = "0" + reportMonth.toString();
+            } else {
+                monthValue = reportMonth.toString();
             }
-        };
-        return [body];
+            for (let i = 1; i < totalDays; i++) {
+                let startDate;
+                let endDate;
+
+                if (i.toString().length === 1) {
+                    startDate = "0" + i.toString();
+                } else {
+                    startDate = i.toString();
+                }
+
+                if ((i + 1).toString().length === 1) {
+                    endDate = "0" + (i + 1).toString();
+                } else {
+                    endDate = (i + 1).toString();
+                }
+
+                const body = {
+                    "reportDescription": {
+                        "reportSuiteID": rsid,
+                        "dateFrom": yearValue + "-" + monthValue + "-" + startDate,
+                        "dateTo": yearValue + "-" + monthValue + "-" + endDate,
+                        "metrics": [{
+                            "id": "instances"
+                        }],
+                        "elements": [{
+                            "id": "videoconcurrentviewers",
+                            "top": "2880"
+                        }],
+                        "segments": [{
+                            "id": segmentId
+                        }],
+                        "sortBy": "instances",
+                        "locale": "en_US"
+                    }
+                };
+                bodyArray.push(body);
+            }
+        }
+        return bodyArray;
     },
     init: (userConfig, reportConfig) => {
         if (!userConfig || typeof(userConfig) !== "object" || !reportConfig || typeof(reportConfig) !== "object") {
@@ -70,21 +122,23 @@ window.getReport = {
             return false;
         }
 
-        let requestBodies = window.getReport.requestBody(reportConfig);
+        const requestBodies = window.getReport.requestBody(reportConfig);
         requestBodies.forEach((element) => {
             window.MarketingCloud.makeRequest(userConfig, reportConfig, "Report.Queue", element).then((data) => {
                 const reportID = JSON.parse(data.responseText).reportID;
                 element.reportID = reportID;
+                console.log(element);
                 // get the report
-                window.getReport.fetch(userConfig, reportConfig, element);
+                window.setTimeout(() => {
+                    window.getReport.fetch(userConfig, reportConfig, element);
+                }, 15000);
             }).catch((error) => {
                 console.log(error);
             });
-        })
-            
+        });
+
     },
     fetch: (userConfig, reportConfig, body) => {
-
         window.MarketingCloud.makeRequest(userConfig, reportConfig, "Report.Get", body).then((data) => {
             let retryCount = 0;
             const retryLimit = 3;
@@ -94,8 +148,17 @@ window.getReport = {
                 }, 15000);
                 retryCount++;
             } else if (data.responseText && JSON.parse(data.responseText).report) {
-                const report = window.parseData.returnCSV(JSON.parse(data.responseText));
-                console.log(report);
+                if (window.getReport.reportValue === "") {
+                    window.getReport.reportValue += window.parseData.generateHeader(JSON.parse(data.responseText));
+                    window.setTimeout(() => {
+                        window.fs.writeFile("./reports/" + JSON.parse(data.responseText).report.type + " - " + JSON.parse(data.responseText).report.period + ".csv", window.getReport.reportValue, (err, data) => {
+                            if (err) {
+                                console.log(err);
+                            }
+                        });
+                    }, 10000);
+                }
+                window.getReport.reportValue += window.parseData.generateBody(JSON.parse(data.responseText));
             } else if (retryCount >= retryLimit) {
                 console.log("Error: could not view report after " + retryLimit + " retries!");
             } else {
