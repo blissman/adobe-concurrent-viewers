c27a28d7b26656dced1d267415e6cab30120869e
Bliss Man <blissman@gmail.com>
2019-05-08 08:34:51 -0400

    update reportTemplate for clarity

--- src/config/reportTemplate.js
+++ src/config/reportTemplate.js
@@ -3,7 +3,7 @@ window.report = {
     segmentId: "someSegmentId", // insert your segment id here
     type: "daily", // can be daily or monthly
     month: 5, // 1-12 representing January-December: the month for monthly reports (not used for daily reporting)
-    year: 2019, // the current year
+    year: 2019, // the desired year for monthly reporting
     startDate: "2018-07-09", // your start date in YYYY-MM-DD format
     endDate: "2018-07-10", // your end date in YYYY-MM-DD format
     endpoint: "api.omniture.com" // Adobe's San Jose datacentre (api2.omniture.com = Dallas, api3.omniture.com = London, api4.omniture.com = Singapore)
