ddc393e20275fd42baf3733c7d34b8d212ea1629
Man, Bliss <bliss.man@bellmedia.ca>
2019-05-14 12:47:01 -0400

    add timestamps to the test cases

--- test/businesslogic.spec.js
+++ test/businesslogic.spec.js
@@ -203,11 +203,15 @@ describe("businesslogic", () => {
 
     it("should generate different header values for monthly and daily reports", () => {
         expect(window.parseData.generateHeader(data, report)).toEqual("Type,ranked\nElements,Video Concurrent Viewers\nReport Suite,id,bellmediatsnprod\n,name,TSN - Prod\nPeriod,Thu.  2 May 2019 - Fri.  3 May 2019\nSegments,id,s300008103_5cccaa0d85d04262783da2e6 \n,Name,TSN Live Streams \nData\nTime,Count,URL\n");
-        expect(window.parseData.generateHeader(data, monthlyReport)).toEqual("Type,ranked\nElements,Video Concurrent Viewers\nReport Suite,id,bellmediatsnprod\n,name,TSN - Prod\nPeriod,May - 2019\nSegments,id,s300008103_5cccaa0d85d04262783da2e6 \n,Name,TSN Live Streams \nData\nTime,Count,URL\n");
+        expect(window.parseData.generateHeader(data, monthlyReport)).toEqual("Type,ranked\nElements,Video Concurrent Viewers\nReport Suite,id,bellmediatsnprod\n,name,TSN - Prod\nPeriod,May - 2019\nSegments,id,s300008103_5cccaa0d85d04262783da2e6 \n,Name,TSN Live Streams \nData\nTime,Unix Timestamp,Count,URL\n");
     });
 
     it("should be able to accept multiple segments in an array", () => {
         expect(window.utils.getSegments(["oneSegment"])).toEqual([{"id": "oneSegment"}]);
         expect(window.utils.getSegments(["oneSegment", "twoSegments"])).toEqual([{"id": "oneSegment"}, {"id": "twoSegments"}]);
+    });
+
+    it("should parse the data body into CSV format", () => {
+        expect(window.parseData.generateBody(data)).toEqual("00:24 2019-05-02,1556771040,237,\n00:25 2019-05-02,1556771100,236,\n00:23 2019-05-02,1556770980,236,\n00:26 2019-05-02,1556771160,235,\n");
     });
 });
