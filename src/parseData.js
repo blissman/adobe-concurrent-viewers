05c2919384318ceaeb89398e4799f4dc9d3e7382
Man, Bliss <bliss.man@bellmedia.ca>
2019-06-17 10:14:57 -0400

    update header to use pipes as a delimiter

--- src/parseData.js
+++ src/parseData.js
@@ -2,30 +2,30 @@ const parseData = {
     generateHeader: (data, reportConfig) => {
         const report = data.report;
         let header = "";
-        header += "Type," + report.type + "\n";
-        header += "Elements," + report.elements[0].name + "\n";
-        header += "Report Suite,id," + report.reportSuite.id + "\n";
-        header += ",name," + report.reportSuite.name + "\n";
-        header += "Period,";
+        header += "Type|" + report.type + "\n";
+        header += "Elements|" + report.elements[0].name + "\n";
+        header += "Report Suite|id|" + report.reportSuite.id + "\n";
+        header += "|name|" + report.reportSuite.name + "\n";
+        header += "Period|";
         if (reportConfig && reportConfig.type === "monthly") {
             header += utils.getMonthName(reportConfig.month) + " - " + reportConfig.year + "\n";
         } else {
             header += report.period + "\n";
         }
         if (report.segments) {
-            header += "Segments,id,";
+            header += "Segments|id|";
             report.segments.forEach((element) => {
                 header += element.id + " ";
             });
             header += "\n";
-            header += ",Name,";
+            header += "|Name|";
             report.segments.forEach((element) => {
                 header += element.name + " ";
             });
             header += "\n";
         }
         header += "Data" + "\n";
-        header += "Time,Unix Timestamp,Count,URL,showName,showDescription" + "\n";
+        header += "Time|Unix Timestamp|Count|URL|showName|showDescription" + "\n";
         return header;
     },
     generateBody: (adobe, capiSchedule) => {
